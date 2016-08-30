be2729505777c1e1288ab57240ec41becd83cefb
Amungo <amungo.navigation@gmail.com>
2016-08-30 21:43:38 +0300

    [!] all channels were extracted as 0ch

--- decoder.cpp
+++ decoder.cpp
@@ -17,9 +17,9 @@ void decode_vector(const std::vector<T_IN> &src, std::vector<T_OUT> &out) {
 void decode8_vector(const std::vector<uint8_t> &src, std::vector<int8_t> &out, int out_chan) {
     switch ( out_chan ) {
     case 0: decode_vector<uint8_t, int8_t, 0>( src, out ); break;
-    case 1: decode_vector<uint8_t, int8_t, 0>( src, out ); break;
-    case 2: decode_vector<uint8_t, int8_t, 0>( src, out ); break;
-    case 3: decode_vector<uint8_t, int8_t, 0>( src, out ); break;
+    case 1: decode_vector<uint8_t, int8_t, 1>( src, out ); break;
+    case 2: decode_vector<uint8_t, int8_t, 2>( src, out ); break;
+    case 3: decode_vector<uint8_t, int8_t, 3>( src, out ); break;
     default: throw std::runtime_error("decode8_vector(): invalid channel " + std::to_string(out_chan) );
     }
 }
